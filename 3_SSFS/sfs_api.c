066f512b5ef1b46b9ab5c90f4a65f6b36399597a
Sean Stappas <seanstappas@gmail.com>
2017-03-30 21:51:30 -0400

    Added remove and simple read/write code

--- 3_SSFS/sfs_api.c
+++ 3_SSFS/sfs_api.c
@@ -17,9 +17,9 @@
 #define DIRECTORY_ENTRY_LENGTH 16
 
 typedef struct _inode_t { // total size of inode = 64 bytes
-	int32_t size; // can have negative size (init to -1). Represents size of file, in bytes (can mod for number of blocks...)
-	int32_t direct[NUM_DIRECT_POINTERS]; // init to -1
-	int32_t indirect; // init to -1. Only used for large files
+	int size; // can have negative size (init to -1). Represents size of file, in bytes (can mod for number of blocks...)
+	int direct[NUM_DIRECT_POINTERS]; // init to -1
+	int indirect; // init to -1. Only used for large files
 } inode_t;
 
 typedef struct _inode_block_t { // A block of inodes
@@ -43,31 +43,31 @@ typedef struct _block_t {
 typedef struct _ofd_table_t { // Q: inode index here, or actually copy the inode?
 	int read_pointer_offsets[NUM_FILES];
 	int write_pointers_offsets[NUM_FILES];
-    inode_t inodes[NUM_FILES]; // TODO: Can be pointer to OFD table
-    // TODO: Can have 'free' bit to denote free slot
+    // TODO: Can have 'free' bit to denote free slot (or just -1...)
 
 } ofd_table_t;
 
 typedef struct _directory_entry_t { // fits within 16 bytes (actually 14)
-	uint32_t inode_index; // 4 bytes
+	int inode_index; // 4 bytes
 	char filename[MAX_FILENAME_LENGTH]; // 10 bytes
 } directory_entry_t;
 
-typedef struct _directory_block_t { // A block of directory entries (root directory)
-	directory_entry_t directory_entries[NUM_DIRECTORY_ENTRIES_PER_BLOCK];
-} directory_block_t;
+typedef struct _root_directory_t { // A block of directory entries (root directory)
+	directory_entry_t directory_entries[NUM_FILES];
+} root_directory_t;
 
 // TODO: Put all the inodes in memory in mkssfs (inode_table) -> write to disk
 
 typedef struct _inode_table_t {
     inode_t inodes[NUM_FILES];
-};
+} inode_table_t;
 
 super_block_t super; // Defines the file system geometry
 block_t fbm; // Unused data blocks (doesn't track super, fbm or wm) (value 1 = data block at that index is unused)
 block_t wm; // Writeable data blocks (value 1 = data block at that index is writeable). Not necessary for simple system (without shadowing)
 ofd_table_t ofd_table; // Open File Descriptor Table (in-memory cache of RW pointers + inodes).
-directory_entry_t directory_cache[NUM_FILES]; // Cache of all filenames
+root_directory_t root_directory; // Cache of all filenames
+inode_table_t inode_table;
 
 void write_single_block(int start_address, void *data, int offset) {
 	void *buf = calloc(1, BLOCK_SIZE); // Allocate a blank block
@@ -118,12 +118,17 @@ void init_super() { // populate root j-node
 	write_single_block(0, &super, 0);
 }
 
-void init_directory_cache() {
-
+void init_root_directory() {
+    for (int i = 0; i < NUM_FILES; i++) {
+        root_directory.directory_entries[i].inode_index = -1;
+    }
 }
 
 void init_ofd() {
-
+    for (int i = 0; i < NUM_FILES; i++) {
+        ofd_table.read_pointer_offsets[i] = -1;
+        ofd_table.write_pointers_offsets[i] = -1;
+    }
 }
 
 /*
@@ -134,14 +139,15 @@ void init_ofd() {
 */
 void mkssfs(int fresh) {
 	char *disk_name = "seanstappas";
-	if (fresh)
-		init_fresh_disk(disk_name, BLOCK_SIZE, NUM_DATA_BLOCKS + 3); // +3 for super, fbm, wm
-	else
-		init_disk(disk_name, BLOCK_SIZE, NUM_DATA_BLOCKS + 3);
-	init_fbm_and_wm(); // What to do here if not fresh? Copy super, fbm, wm from disk.
-	init_super();
-    init_ofd();
-    init_directory_cache();
+	if (fresh) {
+        init_fresh_disk(disk_name, BLOCK_SIZE, NUM_DATA_BLOCKS + 3); // +3 for super, fbm, wm
+        init_fbm_and_wm(); // What to do here if not fresh? Copy super, fbm, wm from disk.
+        init_super();
+        init_ofd();
+        init_root_directory();
+    } else {
+        init_disk(disk_name, BLOCK_SIZE, NUM_DATA_BLOCKS + 3);
+    }
 	// init file containing all i-nodes
 	// setup root directory
 }
@@ -163,8 +169,8 @@ int ssfs_fopen(char *name) {
 	// Return read/write pointer
 
     for (int i = 0; i < NUM_FILES; i++) {
-        if (strcmp(directory_cache[i].filename, name) == 0)
-            return directory_cache[i].inode_index;
+        if (root_directory.directory_entries[i].inode_index != -1 && strcmp(root_directory.directory_entries[i].filename, name) == 0)
+            return root_directory.directory_entries[i].inode_index;
     }
 
 	return -1;
@@ -178,7 +184,8 @@ int ssfs_fopen(char *name) {
 	Returns: 0 on success, -1 on failure.
 */
 int ssfs_fclose(int fileID) {
-    // Removes entry from OFD
+    ofd_table.read_pointer_offsets[fileID] = -1;
+    ofd_table.write_pointers_offsets[fileID] = -1;
 	return -1;
 }        
 
@@ -242,7 +249,7 @@ int ssfs_fwrite(int fileID, char *buf, int length) {
     /*
      * Implement read first! Need to read first before writing.
      *
-     * Check first if at end lf file
+     * Check first if at end of file
      *
      * char giant_buf* = calloc(1, num_blocks * 1024);
      * fseek_read(...);
@@ -268,6 +275,10 @@ int ssfs_fwrite(int fileID, char *buf, int length) {
 */
 int ssfs_fread(int fileID, char *buf, int length) {
 	int read_pointer = ofd_table.read_pointer_offsets[fileID];
+
+    // Assuming single block
+    char *block_temp_buf = read_single_block(inode_table.inodes[fileID].direct[0]);
+    memcpy(buf, block_temp_buf + read_pointer, length);
 
     // Error check!
 
@@ -297,6 +308,24 @@ int ssfs_fread(int fileID, char *buf, int length) {
 */
 int ssfs_remove(char *file) {
     // Set all blocks used by file to FREE (in FBM) and remove inode from inode table.
+    for (int i = 0; i < NUM_FILES; i++) {
+        int inode_index = root_directory.directory_entries[i].inode_index;
+        if (inode_index != -1 && strcmp(root_directory.directory_entries[i].filename, file) == 0) {
+            root_directory.directory_entries[i].inode_index = -1;
+            ofd_table.read_pointer_offsets[inode_index] = -1;
+            ofd_table.write_pointers_offsets[inode_index] = -1;
+            inode_t inode = inode_table.inodes[inode_index];
+            for (int j = 0; j < NUM_DIRECT_POINTERS; j++) {
+                int block_number = inode.direct[j];
+                if (block_number != -1) {
+                    fbm.bytes[block_number] = 1;
+                }
+            }
+            inode_table.inodes[inode_index].size = -1;
+            return 0;
+        }
+    }
+    
 	return -1;
 }         
 
